7bade8a9fbb230a05e4031db2484aa3cb9e40227
rodrigox98 <rodrigo.silva31@hotmail.com>
2022-10-26 09:35:23 -0300

    Inicialização, model e controller

---
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using FilmesAPI.Models;
+using System;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] //faz referência ao próprio nome da classe
+    public class FilmesController : ControllerBase
+    {
+        private static List<Filmes> filmes = new List<Filmes>();
+        private static int Id = 1;
+        [HttpPost]               //O filme vem do corpo da requisição
+        public IActionResult AdcionaFilme([FromBody] Filmes filme)
+        {
+            filme.Id = Id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme);
+        }
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);  
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {   /*  Lógica por trás do 'firstOrDefault'
+            foreach(Filmes filme in filmes)
+            {
+                if(filme.Id == id) return filme;
+            }
+            return null;
+            */                           //Faz referência a objeto do tipo 'Filmes' por é o que contém dentro da list
+            Filmes objetoFilme = filmes.FirstOrDefault(f => f.Id == id);
+
+            if(objetoFilme != null)
+            {
+                return Ok(objetoFilme);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filmes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+
+{
+    public class Filmes
+    {
+        [Required(ErrorMessage = "O campo titulo é obrigatório!")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório!")]
+        public string Diretor { get; set; }
+        [StringLength(50, ErrorMessage = "O tamanho máximo do texto é 50 caracteres")]
+        public string Genero { get; set; }
+        [Range(1,600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+        public int Id { get; set; }
+    }
+}
